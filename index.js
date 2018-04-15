74fe9cb79bff30c5d164c2a713ed7756afd9a4c7
Justin Tooley <jstooley823@gmail.com>
2018-04-15 22:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let store = {drivers:[], passengers:[], trips:[]}
 class Driver {
 
   let id_counter = 0;
@@ -22,3 +23,45 @@ class Driver {
   
 }
 
+class Passanger {
+  let id_counter = 0;
+  constructer(name){
+    this.id = ++id_counter;
+    this.name = name;
+    store_passangers.push(this)
+  }
+
+  trips(){
+    return store.trips.find(trip => {
+      return trip.passanger_id === this.id;
+    })
+  }
+
+  drivers(){
+    return this.trips().map(trip => {
+      return trip.driver()
+    })
+  }
+}
+
+class Trip {
+  let id_counter = 0;
+  constructer(driver,pass){
+    this.driver_id = driver.id
+    this.passanger_id = pass.id
+    this.id = ++id_counter
+    store.trips.push(this)
+  }
+
+  driver(){
+    return store.drivers.find(driver => {
+      return driver.id === this.driver_id;
+    });
+  }
+
+  passenger(){
+    return store.passengers.find(passenger =>{
+      return passenger.id === this.passenger_id;
+    })
+  }
+}
