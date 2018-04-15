fc045e32405069a534c52269edad18f0a1bd5188
Justin Tooley <jstooley823@gmail.com>
2018-04-15 22:33:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 let store = {drivers:[], passengers:[], trips:[]}
 
-let driverId = 0;
-let passengerId = 0;
-let tripId = 0;
-
+let driverId = 0
 class Driver {
   constructor(name) {
     this.id = ++driverId;
@@ -18,7 +15,7 @@ class Driver {
   }
 }
 
-
+let passengerId = 0;
 class Passenger {
   constructor(name) {
     this.id = ++passengerId;
@@ -33,7 +30,7 @@ class Passenger {
   }
 }
 
-let tripId = 0;
+let tripId = 0
 class Trip {
   constructor(driver, passenger){
     this.id = ++tripId
