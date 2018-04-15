2a304152f4c491913db9bec8d3617e05aad41a08
Justin Tooley <jstooley823@gmail.com>
2018-04-15 22:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let store = {drivers:[], passengers:[], trips:[]}
 let driverId = 0
 class Driver {
   constructor(name) {
-    this.id = ++driverId
-    this.name = name
-    store.drivers.push(this)
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
   };
   trips(){
     return store.trips.filter(trip => {return trip.driverId = this.id})
