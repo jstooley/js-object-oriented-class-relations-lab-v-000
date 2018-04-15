b3609a7e330a6b2f8b65a01d482bae9dae35c2e5
Justin Tooley <jstooley823@gmail.com>
2018-04-15 22:29:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,67 +1,50 @@
 let store = {drivers:[], passengers:[], trips:[]}
+
+let driverId = 0
 class Driver {
-
-  let id_counter = 0;
-
-  constructor (name )){
-    this.name = name;
-    this.id = ++id_counter;
-    store.drivers.push(this);
-  }
-
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+  };
   trips(){
-    return store.trips.find(trips => {
-      return trips.driver_id === this.id;
-    })
-  }
-
+    return store.trips.filter(trip => {return trip.driverId = this.id})
+  };
   passengers(){
-    return this.trips().map(trip => {
-      return trip.passenger();
-    });
-  }
+    return store.passengers.filter(passenger => {return passenger.driverId = this.id})
+  };
+};
 
-}
-
-class Passanger {
-  let id_counter = 0;
-  constructor(name){
-    this.id = ++id_counter;
-    this.name = name;
-    store_passangers.push(this)
-  }
-
+let passengerId = 0
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  };
   trips(){
-    return store.trips.find(trip => {
-      return trip.passanger_id === this.id;
-    })
-  }
-
+    return store.trips.filter(trip => {return trip.passengerId = this.id})
+  };
   drivers(){
-    return this.trips().map(trip => {
-      return trip.driver()
-    })
-  }
-}
+    return store.drivers.filter(driver => {return driver.passengerId = this.id})
+  };
+};
 
+let tripId = 0
 class Trip {
-  let id_counter = 0;
-  constructor(driver,pass){
-    this.driver_id = driver.id
-    this.passanger_id = pass.id
-    this.id = ++id_counter
+  constructor(driver, passenger){
+    this.id = ++tripId
+      if(driver){
+      this.driverId = driver.id
+    } if(passenger){
+      this.passengerId = passenger.id
+    }
     store.trips.push(this)
-  }
-
-  driver(){
-    return store.drivers.find(driver => {
-      return driver.id === this.driver_id;
-    });
-  }
-
+  };
   passenger(){
-    return store.passengers.find(passenger =>{
-      return passenger.id === this.passenger_id;
-    })
-  }
-}
+    return store.passengers.find(passenger => {return passenger.id === this.passengerId})
+  };
+  driver(){
+    return store.drivers.find(driver => {return driver.id === this.driverId})
+  };
+};
