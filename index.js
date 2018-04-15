915259b231c25c97e6a9b1837ebbe9fe77c54598
Justin Tooley <jstooley823@gmail.com>
2018-04-15 22:10:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 class Driver {
   let id_counter = 0;
   constructer (name )){
-    this.name = name
-    this.id = ++id_counter
+    this.name = name;
+    this.id = ++id_counter;
   }
 }
 class Passanger {
   let id_counter = 0;
-  constructer(name, ){
-
+  constructer(name){
+    this.id = id_counter;
   }
 }
 
